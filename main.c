9c1a2eccfc0275231ba9a71a2a97ab22c0721bee
adhamabdellatif <adamabdellatief@gmail.com>
2021-08-14 16:37:27 +0200

    change

--- main.c
+++ main.c
@@ -1,8 +1,62 @@
-#include <stdio.h>
 #include <stdlib.h>
-
+    int arr[5];
+    int i,j,swap;
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+        for(i=0;i<5;i++)
+        {
+            printf("enter num ");
+            printf("%d\n",i);
+            scanf("%d",&arr[i]);
+        }
+
+    for(i=0;i<5;i++)
+    {
+     for(j=i+1;j<5;j++)
+     {
+         if(arr[i]>arr[j])
+         {
+             swap=arr[i];
+             arr[i]=arr[j];
+             arr[j]=swap;
+         }
+     }}
+     printf("array in ascending\n");
+     for(i=0;i<5;i++)
+     {
+         printf("%d\t",arr[i]);
+     }
+
+        int max;
+        int y;
+        max=arr[0];
+        for(i=1;i<5;i++)
+        {
+            if(arr[i]>max)
+            {
+                max=arr[i];
+                y=i;
+            }
+        }
+        printf("\nmaximum value in array =");
+        printf("%d\t",max);
+        printf("in position");
+        printf("%d\n",y);
+        int mini ;
+        int x=0;
+        mini=arr[0];
+        for(i=1;i<5;i++)
+        {
+            if(arr[i]<mini)
+            {
+                mini=arr[i];
+                x=i;
+            }
+        }
+        printf("minimum value in array =");
+        printf("%d\t",mini);
+        printf("in position");
+        printf("%d\n",x);
+        return 0;
 }
+
